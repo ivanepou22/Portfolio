ebf0bd3b2963ad2cf96977f04299d1b9b31ea4b0
Ivan Epou <ivanepou92@gmail.com>
2021-09-01 00:39:20 +0300

    add dark and light mode

--- app.js
+++ app.js
@@ -5,3 +5,32 @@ menu.addEventListener('click', () => {
     menu.classList.toggle('is-active');
     menuinks.classList.toggle('active');
 })
+
+window.onunload = (e) => {
+    window.localStorage.setItem('theme', 'dark');
+}
+
+//body class
+let bodythemeClass = document.querySelector('body');
+//checkbox value
+let checkbox = document.getElementById('theme-color');
+checkbox.addEventListener('change', function(event) {
+    bodythemeClass.classList.remove('dark');
+    bodythemeClass.classList.remove('light');
+    let themeColor = window.localStorage.getItem('theme');
+    if (themeColor == null) {
+        window.localStorage.setItem('theme', 'light');
+        themeColor = window.localStorage.getItem('theme');
+    } else if (themeColor == 'light') {
+        window.localStorage.removeItem('theme')
+        window.localStorage.setItem('theme', 'dark');
+        themeColor = window.localStorage.getItem('theme');
+    }  else if(themeColor = 'dark') {
+        window.localStorage.removeItem('theme');
+        window.localStorage.setItem('theme', 'light');
+        themeColor = window.localStorage.getItem('theme');
+    }
+
+    bodythemeClass.classList.add(themeColor);
+    console.log(bodythemeClass.classList.value);
+})
